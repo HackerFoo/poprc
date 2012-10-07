e4adb3bdd6834d099a85d1d6fc65f389fc256ca3
Dustin DeWeese <dustin.deweese@gmail.com>
2012-10-07 17:17:39 -0400

    fixed license headers

--- PegC/Tokenize.hs
+++ PegC/Tokenize.hs
@@ -1,18 +1,18 @@
 {- Copyright 2012 Dustin DeWeese
-   This file is part of peg.
+   This file is part of pegc.
 
-    peg is free software: you can redistribute it and/or modify
+    pegc is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    peg is distributed in the hope that it will be useful,
+    pegc is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with peg.  If not, see <http://www.gnu.org/licenses/>.
+    along with pegc.  If not, see <http://www.gnu.org/licenses/>.
 -}
 
 module PegC.Tokenize where
