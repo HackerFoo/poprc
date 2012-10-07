fe08eed20e1ca5aec33aa07728a3df4f368b7791
Dustin DeWeese <dustin.deweese@gmail.com>
2012-10-07 16:56:36 -0400

    working on AST

---
+++ PegC/Graph.hs
@@ -0,0 +1,76 @@
+module PegC.Graph where
+
+import PegC.Tokenize
+import PegC.Value
+import Control.Monad.State
+import Data.List
+import Debug.Trace
+{-
+arity = foldl' composeArity (0,0)
+
+composeArity (i0, o0) (i1, o1) | d >= 0 = (i0, o1 + d)
+                               | otherwise = (i0 - d, o1)
+  where d = o0 - i1
+-}
+type ValInfo = (Value, [(Int, Int)])
+type AST = (Int, Int, [(Int, Int)], [ValInfo])
+
+-- dependencies
+ast :: [Value] -> AST
+ast = foldl' (\x v -> op v (arity v) x) (0, 0, [], [])
+      -- c --> output count
+      -- r --> remaining values required
+      -- s --> stack holding locations of outputs
+      -- g --> the graph
+  where op v (i,o) (c, r, s, g) = (c+1, r + r', s'', g')
+          where (a, s') = splitAt i s
+                r' = i - length a
+                s'' = [(0, x) | x <- [0..o-1]] ++ map (\(x, y) -> (x+1, y)) s'
+                g' = (v, a ++ [(c, x) | x <- [r..r+r'-1]]) : g
+
+composeAst :: AST -> AST -> AST
+composeAst (cf, rf, sf, f) (cg, rg, sg, g) =
+  (cf + cg + 1,
+   rf + rg',
+   sg ++ map (\(x, y) -> (x+cg+1, y)) (drop rg sf),
+   g ++ (W "id#", sf ++ [(cf+1,x) | x <- [0..rg'-1]]) : f)        
+  where rg' = max 0 (rg - length sf)
+
+evalA :: AST -> Maybe [Value]
+evalA (c, r, s, g) | r /= 0 = Nothing
+                   | otherwise = Just $ getArgs s vs
+  where vs = foldr (\(x, ds) s -> exec x (getArgs ds s) : s) [] g
+
+eval :: [Value] -> Maybe [Value]
+eval = evalA . ast
+
+-- remove dropped branches
+prune :: AST -> AST
+prune (c, r, s, g) = (length g', r, s', g')
+  where ((_, s'):g') = reverse . snd $ foldl shift ([0], []) ((W "", s):g)
+        shift (s, g) (x, ds) | 0 `elem` s = (map fst ds ++ (filter (>=0) . map (subtract 1) $ s), (x, ds) : g)
+                             | otherwise = (filter (>= 0) . map (subtract 1) $ s, update g)
+        update = zipWith shiftDep [0..] :: [ValInfo] -> [ValInfo]
+          where shiftDep n (x, ds) = (x, map (\(d, y) -> if d > n then (d-1, y) else (d, y)) ds) :: ValInfo
+
+exec (W "+") [I y, I x] = [I $ x + y]
+exec (W "-") [I y, I x] = [I $ x - y]
+exec (W "dup") [x] = [x, x]
+exec (W "drop") [x] = []
+exec (W "swap") [x, y] = [y, x]
+exec (W "id#") x = x
+exec x [] = [x]
+
+getArgs ds s = map (\(x,y) -> s!!x!!y) ds
+
+arity x@(W "+") = (2,1)
+arity x@(W "-") = (2,1)
+arity x@(W "dup") = (1,2)
+arity x@(W "drop") = (1,0)
+arity x@(W "swap") = (2,2)
+arity x@(W "\\/") = (2,1)
+arity x@(W "!") = (1,1)
+arity x@(W ".") = (2,1)
+arity _ = (0,1)
+
+-- $ forces arg to calculate arity
---
+++ PegC/Value.hs
@@ -0,0 +1,117 @@
+module PegC.Value where
+
+import Data.Maybe
+import Control.Monad
+import Control.Applicative
+
+data Value = F Double  -- float
+           | I Integer -- integer
+           | C Char    -- character
+           | L [Value] -- list
+           | W String  -- word
+           | A String  -- atom
+           | V String  -- variable
+           | S String  -- stack variable
+           | Io        -- I/O token
+  deriving (Show, Eq, Ord)
+
+isWord (W _) = True
+isWord _ = False
+
+isAtom (A _) = True
+isAtom _ = False
+
+isInt (I _) = True
+isInt _ = False
+
+isChar (C _) = True
+isChar _ = False
+
+isList (L _) = True
+isList _  = False
+
+isFloat (F _) = True
+isFloat _ = False
+
+toString :: Value -> Maybe String
+toString (L l) = loop l
+  where loop [] = return ""
+        loop (C c : r) = (c:) <$> loop r
+        loop _ = mzero
+toString _ = mzero
+
+isString = isJust . toString
+
+isIo Io = True
+isIo _ = False
+
+isVar (V _) = True
+isVar _ = False
+
+isStackVar (S _) = True
+isStackVar _ = False
+
+has p (L l) = any (has p) l
+has p x = p x
+
+class ValueT a where
+  toValue :: a -> Value
+  fromValue :: Value -> Maybe a
+
+instance ValueT Double where
+  toValue = F
+  fromValue (F x) = Just x
+  fromValue _ = Nothing
+
+instance ValueT Integer where
+  toValue = I
+  fromValue (I x) = Just x
+  fromValue _ = Nothing
+
+instance ValueT Char where
+  toValue = C
+  fromValue (C x) = Just x
+  fromValue _ = Nothing
+
+instance ValueT Bool where
+  toValue = A . show
+  fromValue (A "True") = Just True
+  fromValue (A "False") = Just False
+  fromValue _ = Nothing
+
+instance (ValueT a) => ValueT [a] where
+  toValue = L . map toValue
+  fromValue (L x) = mapM fromValue x
+  fromValue _ = Nothing
+
+ofType :: (ValueT a) => Value -> a -> Bool
+ofType v x = isJust $ fromValue v `asTypeOf` Just x
+
+val :: (ValueT a) => Value -> a
+val = fromJust . fromValue
+
+showsVal (I x) = shows x
+showsVal (C x) = shows x
+showsVal (F x) = shows x
+showsVal (W x) = (x ++)
+showsVal (A x) = (x ++)
+showsVal (V x) = (('?':x) ++)
+showsVal (S x) = (('@':x) ++)
+showsVal Io = ("IO" ++)
+showsVal (L []) = ("[ ]" ++)
+showsVal (L x) = case toString (L x) of
+  Just str -> (str ++)
+  Nothing -> case toQuote (L x) of
+    Just str -> (str ++)
+    Nothing -> (("[ " ++ showsValues x " ]") ++)
+
+showsValues [] = id
+showsValues [x] = showsVal x
+showsValues (x:xs) = showsVal x . (' ':) . showsValues xs
+
+showValues x = showsValues x ""
+showVal x = showsVal x ""
+
+toQuote (L [x]) | isList x = ('`':) <$> toQuote x
+                | otherwise = Just ('`' : showVal x)
+toQuote _ = Nothing
