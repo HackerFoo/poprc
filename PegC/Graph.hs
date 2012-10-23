ec884adff10d2089b2e72e275a7c1eebc36c27f7
Dustin DeWeese <dustin.deweese@gmail.com>
2012-10-23 05:51:38 -0400

    added composition

--- PegC/Graph.hs
+++ PegC/Graph.hs
@@ -57,7 +57,7 @@ pushI x = do (a, s, c) <- get
              put (a, x:s, c)
              return x
 pop = do (a, x:s, c) <- get
-         put $ trace ("B (s,c) = " ++ show (s,c)) (a, s, c)
+         put (a, s, c)
          return x
 swap = do (a, x:y:s, c) <- get
           put (a, y:x:s, c)
@@ -66,8 +66,7 @@ dup = do (a, x:s, c) <- get
          put (a, x:x:s, c)
          return [x,x]
 
-regCnt (L xs) = ic + sum (map regCnt xs) + 1
-  where (ic, oc) = arityM xs
+regCnt (L xs) = fst (arityM xs) + sum (map regCnt xs) + 1
 regCnt (W "swap") = 0
 regCnt (W "dup") = 0
 regCnt (W "pop") = 0
@@ -105,32 +104,33 @@ ast (W "popr") = do
   tellA [(y, (0, Q (AST i o),[]))]
   pushI x
   return [x,y]
-  {-
 ast (W ".") = do
-  x <- pop
   y <- pop
+  x <- pop
   (_, Q astX, _) <- getRef x
   (_, Q astY, _) <- getRef y
+  astZ <- composeAst astX astY
   z <- push
-  tellA [(z, (0, composeAst astX astY, []))]
+  tellA [(z, (0, Q astZ, []))]
   return [z]
--}
 ast x = do
     i <- replicateM ic pop
     o <- replicateM oc push
     tellA [(r, (n, x, i)) | (r,n) <- zip o [0..]]
     return o
   where (ic, oc) = arity x
-{-
-composeAst (AST ix ox) (AST iy oy) =
-  where 
--}
+
+composeAst (AST ix ox) (AST iy oy) = do
+  tellA . map (\(o, i) -> (i, (0, R o, []))) $ zip ox iy
+  return (AST (drop (length ox) iy ++ ix) (oy ++ drop (length iy) ox))
+
 buildAst xs = a
   where (a, s, c) = flip execState (array (0, regCnt (L xs) - 1) [], [], 0) . ast . L $ xs
 
 exec (0, W "+", i) = do
   [I x, I y] <- mapM force i
   return . I $ x + y
+exec (0, R x, []) = force x
 exec (0, x, []) = return x
 
 force x = exec . (!x) =<< ask  
@@ -141,82 +141,10 @@ eval a = mapM force o `runReader` a
 
 run xs = (eval . buildAst) `liftM` tok xs
 
-{-
-mapAST f (Node x i ns) = f x i $ mapAST f `map` ns
-mapASTM f (Node x i ns) = f x i $ mapM (mapASTM f) ns
--}
-{-
-swizzle
-  :: (MonadPlus m, MonadState (M.IntMap Value) m) =>
-     AST -> m AST
-swizzle (AST xs rq) = do xs' <- mapM (mapASTM f) xs
-                         return $ AST xs' rq
-  where f (W "dup") _ m = liftM head m
-        f (W "id") n m = do xs <- m
-                            guard (n < length xs)
-                            return (xs !! n)
-        f (W "swap") n m = do xs <- m
-                              guard (n < length xs)
-                              return (reverse xs !! n)
-        f (W "popr") 0 m = do [q] <- m
-                              Q (AST (x:xs) 0) <- mapASTM exec q
-                              return (moveIn (-1) x)
-        f (W "popr") 1 m = do [q] <- m
-                              Q (AST (x:xs) 0) <- mapASTM exec q
-                              return $ Node (Q $ AST xs 0) 0 []
-        f (W "quot") _ m = do [x] <- m
-                              return $ Node (Q $ AST [moveIn 1 x] 0) 0 []
-        f (Q ast) i m = do ast' <- swizzle ast
-                           Node (Q ast') i `liftM` m
-        f v i m = Node v i `liftM` m
-        
-moveIn y (Node (In l) n c) = Node (In $ l + y) n (map (moveIn y) c)
-moveIn y (Node x n c) = Node x n (map (moveIn y) c)
--}                
 tok x = case tokenize x of
   Left _ -> error "failed to tokenize"
   Right x -> return x
 {-
-eval (AST xs 0) = mapM (mapASTM exec) xs
-eval (AST _ _) = error "not enough arguments"
-
-composeAST (AST x rx) (AST y ry) = AST (map (mapAST (f 0)) y ++ drop ry x) (rx + max 0 (ry - length x))
-  where f l (In l') n [] | l == l' = if n < length x
-                                        then x!!n
-                                        else Node (In l) (n - length x) []
-        f l (Q (AST ns 0)) n [] = Node (Q (AST (map (mapAST (f (l+1))) ns) 0)) n [] 
-        f _ v i xs = Node v i xs
-
-exec (W "+") _ m = do [I y, I x] <- m
-                      return . I $ x + y
-exec (W "-") _ m = do [I y, I x] <- m 
-                      return . I $ x - y
-exec (W "*") _ m = do [I y, I x] <- m 
-                      return . I $ x * y
-exec (W "div") _ m = do [I y, I x] <- m
-                        return . I $ x `div` y
-exec (W ".") _ m = do [Q y, Q x] <- m
-                      return . Q $ composeAST x y
-exec (W "dup") _ m = do [x] <- m
-                        return x
-exec (W "id") n m = do xs <- m
-                       guard $ n < length xs
-                       return $ xs !! n
-exec (W "swap") n m = do xs <- m
-                         guard $ n < length xs
-                         return $ reverse xs !! n
-exec (R x) _ y = do m <- get
-                    case M.lookup x m of
-                      Nothing -> do [z] <- y
-                                    case z of
-                                      In _ -> return z
-                                      _ -> do
-                                        modify (M.insert x z)
-                                        return z
-                      Just z -> return z
-exec (W _) _ _ = error "unknown word"
-exec x _ _ = return x
-
 varName x = "var" ++ show x
 
 gen (W "+") _ b = do [x, y] <- b
@@ -242,12 +170,6 @@ gen (W w) x b = do cs <- b
                    tell [varName c ++ " = " ++ w ++ "(" ++ commas (map varName cs) ++ ")"]
                    put (m, c+1, i)
                    return c
-
-comp x = do (t, _) <- ast 0 `liftM` tok x
-            a <- swizzle t
-            return a
-comp' = flip evalStateT M.empty . comp
-run = flip evalStateT M.empty . (eval <=< comp)
 
 commas = concat . intersperse ", "
 
