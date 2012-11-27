46c99349420e891edccb84c0ed221c4b58decab3
Dustin DeWeese <dustin.deweese@gmail.com>
2012-11-27 00:33:05 -0500

    optimized argument insertion

--- rt.c
+++ rt.c
@@ -2,17 +2,20 @@
 #include <strings.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef struct closure closure_t;
 struct closure {
   int (*func)(closure_t *);
   union {
-    int val;
+    intptr_t val;
     closure_t *in[1];
   };
 };
 
 #define LENGTH(_a) (sizeof((_a)[0]) / sizeof(_a))
+// make sure &cl_stack > 255
+uint8_t padding[256];
 closure_t cl_stack[1024];
 int top = 0;
 
@@ -30,13 +33,21 @@ closure_t *val(int x) {
   return &cl_stack[top++];
 }
 
+// max offset is 255
+bool is_offset(closure_t *c) {
+  return !((intptr_t)c & ~0xff);
+}
+
 closure_t *func(int (*f)(closure_t *), int args) {
   int cur = top;
   int size = 1 + ((sizeof(closure_t *) * (args - 1) + sizeof(closure_t) - 1) / sizeof(closure_t));
   bzero(&cl_stack[cur], sizeof(closure_t)*size);
+  /*
   if((void*)(&cl_stack[cur].in[args]) < (void*)(&cl_stack+1))
     cl_stack[cur].in[args] = &cl_stack[-1]; // invalid nonzero value to mark end of arguments
+  */
   cl_stack[cur].func = f;
+  cl_stack[cur].in[0] = (closure_t *)(intptr_t)args;
   top += size;
   return &cl_stack[cur];
 }
@@ -56,30 +67,48 @@ int add(closure_t *c) {
   return c->val;
 }
 
-// destructive composition
-bool comp(closure_t *c, closure_t *a) {
+bool arg(closure_t *c, closure_t *a) {
   if(c->func == noop) return false;
-  closure_t **p = c->in;
-  while(is_closure(*p)) {
-    if(comp(*p, a)) return true;
-    ++p;
-  }
-  if(!*p) {
-    *p = a;
-    return true;
-  }
-  return false; 
+
+  // c->in[0] is an offset to the next arg
+  // until it is replaced with a closure
+  if(is_closure(c->in[0]) ||
+     !(intptr_t)c->in[0]) return false;
+  intptr_t o = --*((intptr_t *)&c->in[0]);
+  c->in[o] = a;
+  return true;
 }
 
 int closure_size(closure_t *c) {
   if(c->func == noop) return 0;
   closure_t **p = c->in;
   int n = 0;
-  while(!*p || is_closure(*p)) {
+  if(is_offset(*p)) {
+    intptr_t o = (intptr_t)(*p);
+    p += o;
+    n += o;
+  }
+  while(is_closure(*p)) {
     p++;
     n++;
   }
   return n;
+}
+
+// destructive composition
+bool comp(closure_t *c, closure_t *a) {
+  if(c->func == noop) return false;
+  int i = closure_size(c) - 1;
+  while(i >= 0 && is_closure(c->in[i])) {
+    if(comp(c->in[i], a)) return true;
+    --i;
+  }
+  if(i >= 0) {
+    c->in[0] = (closure_t *)(intptr_t)i;
+    c->in[i] = a;
+    return true;
+  }
+  return false; 
 }
 
 closure_t *dup(closure_t *c) {
@@ -106,23 +135,12 @@ closure_t *dup(closure_t *c) {
   return tmp;
 }
 
-bool arg(closure_t *c, closure_t *a) {
-  if(c->func == noop) return false;
-  closure_t **p = c->in;
-  while(!*p || is_closure(*p)) ++p;
-  if(!*p) {
-    *p = a;
-    return true;
-  }
-  return false;
-}
-
 inline
 int force(closure_t *c) {
   c->func(c);
 }
 
-void main() {
+int main() {
   closure_t *a, *b, *c, *d, *e, *f;
 
   a = val(1);
@@ -140,5 +158,6 @@ void main() {
   printf("c = %d\n", force(c));
   printf("e = %d\n", force(e));
   printf("f = %d\n", force(f));
-  printf("closure_size() = %d\n", closure_size(e));
+  printf("closure_size() = %d\n", closure_size(f));
+  return 0;
 }
