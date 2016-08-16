f24aa9a6f85b74410f5604fbe1c5f6da60844e04
Dustin DeWeese <dustin.deweese@gmail.com>
2016-08-15 20:27:17 -0700

    working on light pushl/popr concept

--- cgen/quote.c
+++ cgen/quote.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <assert.h>
 #include "macros.h"
 
 // trying out an idea for lightweight quotes
 
 typedef union slot slot_t;
 
-typedef slot_t *(*slot_fn)(slot_t *);
+typedef slot_t *(*popr_fn)(slot_t *, intptr_t *);
 
 union slot {
   intptr_t val;
   slot_t *arg;
-  slot_fn fn;
+  popr_fn popr;
 };
 
 slot_t slots[1<<8] = {0};
@@ -24,27 +25,41 @@ slot_t *new_slots(size_t n) {
   return x;
 }
 
-slot_t *reduce(slot_t *x) {
-  return (x->fn)(x);
+slot_t *pushl(slot_t *x, slot_t *s) {
+  assert(!s->arg);
+  s->arg = x;
+  return s - 1;
 }
 
-#define DEF_FN_2(name)                   \
-  slot_t *fn_##name(slot_t *s) {         \
-    slot_t                               \
-      *r = new_slots(1),                 \
-      *x = reduce(s[1].arg),             \
-      *y = reduce(s[2].arg);             \
-    r[0].val = name(x->val, y->val);     \
-    return r;                            \
+slot_t *popr(slot_t *s, intptr_t *v) {
+  if(s->popr) {
+    return s->popr(s, v);
+  } else { // id
+    s++;
+    *v = s->val;
+    s->popr = 0;
+    return s;
+  }
 }
 
-#define DEF_FN_1(name)            \
-  slot_t *fn_##name(slot_t *s) {  \
-    slot_t                        \
-      *r = new_slots(1),          \
-      *x = reduce(s[1].arg);      \
-    r[0].val = name(x->val);      \
-    return r;                     \
+#define DEF_FN_21(name)                       \
+  slot_t *fn_##name(slot_t *s, intptr_t *v) { \
+    intptr_t x, y;                            \
+    popr(s[1].arg, &x);                       \
+    popr(s[2].arg, &y);                       \
+    *v = name(x, y);                          \
+    s->popr = 0;                              \
+    return s;                                 \
+  }
+
+
+#define DEF_FN_11(name)                       \
+  slot_t *fn_##name(slot_t *s, intptr_t *v) { \
+    intptr_t x;                               \
+    popr(s[1].arg, &x);                       \
+    *v = name(x);                             \
+    s->popr = 0;                              \
+    return s;                                 \
 }
 
 #define DEF_OP(name, op)                        \
@@ -60,41 +75,37 @@ intptr_t negate(intptr_t x) {
   return -x;
 }
 
-slot_t *fn_id(slot_t *s) {
-  return s + 1;
-}
-
-DEF_FN_1(negate);
-DEF_FN_2(add);
-DEF_FN_2(sub);
-DEF_FN_2(mul);
+DEF_FN_11(negate);
+DEF_FN_21(add);
+DEF_FN_21(sub);
+DEF_FN_21(mul);
 
 slot_t *make_closure(char c) {
   slot_t *ret = NULL;
   if(c >= '0' && c <= '9') {
     ret = new_slots(2);
-    ret[0].fn = fn_id;
+    ret[0].popr = 0;
     ret[1].val = c - '0';
   } else {
     switch(c) {
     case '+':
       ret = new_slots(3);
-      ret[0].fn = fn_add;
+      ret[0].popr = fn_add;
       ret += 2;
       break;
     case '-':
       ret = new_slots(3);
-      ret[0].fn = fn_sub;
+      ret[0].popr = fn_sub;
       ret += 2;
       break;
     case '*':
       ret = new_slots(3);
-      ret[0].fn = fn_mul;
+      ret[0].popr = fn_mul;
       ret += 2;
       break;
     case 'n':
       ret = new_slots(2);
-      ret[0].fn = fn_negate;
+      ret[0].popr = fn_negate;
       ret += 1;
       break;
     }
@@ -102,20 +113,16 @@ slot_t *make_closure(char c) {
   return ret;
 }
 
-slot_t *pushl(slot_t *x, slot_t *s) {
-  s->arg = x;
-  return s - 1;
-}
-
 int main(int argc, char **argv) {
   slot_t
     *x1 = make_closure('1'),
     *x2 = make_closure('2'),
     *add = make_closure('+'),
-    *gr = pushl(x1, pushl(x2, add)),
-    *r = reduce(gr);
+    *r = pushl(x1, pushl(x2, add));
+  intptr_t val;
+  r = popr(r, &val);
 
-  printf("r->val = %d\n", (int)r->val);
+  printf("val = %d\n", (int)val);
   
   return 0;
 }
