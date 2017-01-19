967250c61e170161c891327a59b82d5d3d44ae4c
Dustin DeWeese <dustin.deweese@gmail.com>
2017-01-19 05:13:41 -0800

    fix a parsing bug

--- parse.c
+++ parse.c
@@ -477,9 +477,13 @@ cell_t *parse_expr(const cell_t **l, cell_t *module) {
     case CC_SYMBOL:
       if(seg.n == 1 && *seg.s == ',') {
         if(n == 1) {
-          arg_stack[1] = arg_stack[0];
-          arg_stack[0] = &nil_cell;
-          n = 2;
+          if(closure_is_ready(arg_stack[0])) {
+            arg_stack[1] = arg_stack[0];
+            arg_stack[0] = &nil_cell;
+            n = 2;
+          } else {
+            arg(&arg_stack[0], &nil_cell);
+          }
         }
         arg_stack[0] = array_to_list(arg_stack, n);
         n = 1;
