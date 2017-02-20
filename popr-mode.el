00a43f7d7a54ee2db10aaabaf8da9a073b99a8a6
Dustin DeWeese <dustin.deweese@gmail.com>
2017-02-20 11:36:25 -0800

    work on library and popr-mode

--- popr-mode.el
+++ popr-mode.el
@@ -12,11 +12,12 @@
   :group 'faces)
 
 (defvar popr-highlights
-      '(("__ .*". font-lock-comment-face)
-        ("_\\w.*\\w_" . font-lock-comment-face)
+      '(("__+ .*". font-lock-comment-face)
+        ("_+\\w.*\\w_+" . font-lock-comment-face)
         ("module\\|imports" . font-lock-keyword-face)
         ("\\[\\|\\]" . popr-bracket-face)
-        ("^\\s-*\\(\\w+\\):" . font-lock-function-name-face)))
+        ("^\\s-*\\(\\w+\\):" . font-lock-function-name-face)
+        ("\\b[A-Z][A-Za-z0-9]+\\b\\|\\b[0-9]+\\b" . font-lock-constant-face)))
 
 (define-derived-mode popr-mode fundamental-mode "popr"
   "Major mode for editing Popr code."
