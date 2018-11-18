e0dd72cb4359cab4688d52b9df3ba80e2406dec7
Dustin DeWeese <dustin.deweese@gmail.com>
2018-11-18 12:36:02 -0800

    update scripts

--- startle/startle.mk
+++ startle/startle.mk
@@ -11,7 +11,7 @@ print-%:
 # generate dependency info and headers
 $(BUILD_DIR)/%.d: %.c
 	@mkdir -p $(dir $@)
-	@$(CC) $(INCLUDE) -MM $(CFLAGS) $*.c -MG -MT $(BUILD_DIR)/$*.o -o- | \
+	@$(CC) $(INCLUDE) -MM $(CFLAGS) $*.c -MG -MT $(BUILD_DIR)/$*.o -o- 2>/dev/null | \
 	  sed -E -e 's/ ([a-zA-Z][^ .]*)\.h/ .gen\/\1.h/g' > $(BUILD_DIR)/$*.d
 
 LOCAL_HEADERS += $(shell find -L . -not -path './.gen/*' -name '*.h')
