e810e3abab661ef733290a1363f72baa30f1d2dd
Dustin DeWeese <dustin.deweese@gmail.com>
2019-02-25 04:01:57 -0800

    fix escaping in git_log.h

--- startle/gen.mk
+++ startle/gen.mk
@@ -11,7 +11,7 @@ SHELL := bash
 .gen/%_list.h.new: NAME=$(shell echo $(notdir $*) | tr a-z A-Z)
 .gen/%_list.h.new: $(SRC)
 	@mkdir -p $(dir $@)
-	grep -n "$(NAME)" $(SRC) | \
+	grep -n '$(NAME)' $(SRC) | \
 	  sed -e 'h;s/\(.*\):.*/\1/;y/\//_/;G;s/\n.*:/:/' | \
 	  sed -E -n -e 's/^(.*)\.c:(.*): *'"$(NAME)"'(_[A-Z][A-Z_]*)?\((.*)\).*/'"$(NAME)\3__ITEM"'(\1, \2, \4)/p' | \
 	  LC_ALL=C sort -t ',' -u -k 3,3 > $@
@@ -21,9 +21,9 @@ SHELL := bash
 .gen/git_log.h.new: $(SRC)
 	@mkdir -p $(dir $@)
 	@if git diff-index --quiet HEAD --; then \
-		echo "#define GIT_LOG \"$(LOG)\"" > $@; \
+		echo '#define GIT_LOG "$(LOG)"' > $@; \
 	else \
-		echo "#define GIT_LOG \"$(LOG) [DIRTY]\"" > $@; \
+		echo '#define GIT_LOG "$(LOG) [DIRTY]"' > $@; \
 	fi
 
 .gen/%-local.h.new: %.c startle/bin/makeheaders
