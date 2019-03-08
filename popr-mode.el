10f40e6de060319d151e995bd4d33b0bba94d1e1
Dustin DeWeese <dustin.deweese@gmail.com>
2019-03-08 05:13:10 -0800

    fix comment highlighting in popr-mode

--- popr-mode.el
+++ popr-mode.el
@@ -12,10 +12,10 @@
   :group 'faces)
 
 (defvar popr-highlights
-      '(("\\(^\\| \\)\\(_+[^_[:space:]].*\\(\n.*?\\)*.*[^_[:space:]]_+\\)\\|\\(__+ .*$\\)" . font-lock-comment-face)
+      '(("\\(?:^\\| \\)\\(_+[^_[:space:]].*\\(\n.*?\\)*.*[^_[:space:]]_+[[:space:]]\\|__+ .*$\\)" 0 font-lock-comment-face t)
         ("\\<\\(module\\|imports\\)\\>" . font-lock-keyword-face)
         ("\\[\\|\\]" . popr-bracket-face)
-        ("\\([^[:space:]]+\\)\\s-*:\\s-" . font-lock-function-name-face)
+        ("\\(^ *[^[:space:]]+\\)\\s-*:" . font-lock-function-name-face)
         ("\\<\\([A-Z][A-Za-z0-9]+\\b\\|\\b[0-9]+\\)\\>" . font-lock-constant-face)))
 
 (defvar popr-imenu-generic-expression
@@ -25,7 +25,26 @@
 (define-derived-mode popr-mode fundamental-mode "popr"
   "Major mode for editing Popr code."
   (setq font-lock-defaults '(popr-highlights))
-  (setq imenu-generic-expression popr-imenu-generic-expression))
+  (setq imenu-generic-expression popr-imenu-generic-expression)
+  (set (make-local-variable 'font-lock-multiline) t)
+  (add-hook 'font-lock-extend-region-functions
+            'test-font-lock-extend-region)
+)
+
+;; from http://makble.com/emacs-font-lock-how-to-highlight-multiline-text
+(defun test-font-lock-extend-region ()
+  "Extend the search region to include an entire block of text."
+  ;; Avoid compiler warnings about these global variables from font-lock.el.
+  ;; See the documentation for variable `font-lock-extend-region-functions'.
+  (eval-when-compile (defvar font-lock-beg) (defvar font-lock-end))
+  (save-excursion
+    (goto-char font-lock-beg)
+    (let ((found (or (re-search-backward "\n\n" nil t) (point-min))))
+      (goto-char font-lock-end)
+      (when (re-search-forward "\n\n" nil t)
+        (beginning-of-line)
+        (setq font-lock-end (point)))
+      (setq font-lock-beg found))))
 
 (provide 'popr-mode)
 
