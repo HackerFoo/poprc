fbeef41ab02aa0aa2175cf8ab99a961e0974321c
Dusty DeWeese <dustin.deweese@gmail.com>
2020-04-14 07:20:20 -0700

    fix .nix

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ with pkgs;
 stdenv.mkDerivation {
   name = "poprc";
   src = ./.;
-  buildInputs = [ verilog ];
-  buildFlags = [ "testbenches" "PREFIX=$(out)" "EXTRA_CFLAGS=$(NIX_CFLAGS_COMPILE)" "USE_LINENOISE=y" ];
+  buildInputs = [ git verilog readline ];
+  buildFlags = [ "testbenches" "PREFIX=$(out)" "EXTRA_CFLAGS=$(NIX_CFLAGS_COMPILE)" "USE_READLINE=y" ];
   installFlags = [ "PREFIX=$(out)" ];
 }
