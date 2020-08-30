aaf41c6fb6d8122683c4fcb247d28920b3d975fc
Dusty DeWeese <dustin.deweese@gmail.com>
2020-08-30 10:59:45 -0700

    disable Verilog testbench tests because iverilog build is broken

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ with pkgs;
 stdenv.mkDerivation {
   name = "poprc";
   src = ./.;
-  buildInputs = [ git verilog readline ];
-  buildFlags = [ "testbenches" "PREFIX=$(out)" "EXTRA_CFLAGS=$(NIX_CFLAGS_COMPILE)" "USE_READLINE=y" ];
+  buildInputs = [ git readline ];
+  buildFlags = [ "PREFIX=$(out)" "EXTRA_CFLAGS=$(NIX_CFLAGS_COMPILE)" "USE_READLINE=y" ];
   installFlags = [ "PREFIX=$(out)" ];
 }
